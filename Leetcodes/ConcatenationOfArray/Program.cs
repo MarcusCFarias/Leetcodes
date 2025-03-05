81301b3b0addd1fd299423be3764e8d8fcf33e50
Marcus Farias <marcusczupriniakifarias@gmail.com>
2025-03-05 08:43:37 -0300

    Concatenation of Array solution

---
+++ Leetcodes/ConcatenationOfArray/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConcatenationOfArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 1 };
+
+            int[] result = GetConcatenation_FirstAttempt(array);
+
+            Console.Write("Answear: [");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write($"{result[i]},");
+            }
+
+            Console.WriteLine("]");
+        }
+        public static int[] GetConcatenation_FirstAttempt(int[] nums)
+        {
+            int[] output = new int[nums.Length * 2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                output[i] = nums[i];
+                output[i + nums.Length] = nums[i];
+            }
+
+            return output;
+        }
+    }
+}
