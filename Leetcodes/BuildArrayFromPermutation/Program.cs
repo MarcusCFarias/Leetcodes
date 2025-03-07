083bc150309e26f12eff93e9293dc0ba880f9f40
Marcus Farias <marcusczupriniakifarias@gmail.com>
2025-03-07 07:44:51 -0300

    build array form permutation

---
+++ Leetcodes/BuildArrayFromPermutation/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BuildArrayFromPermutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 5, 0, 1, 2, 3, 4 };
+            int[] result = BuildArray(input);
+
+            Console.WriteLine(result);
+            for (int i = 0; i < result.Length; i++)
+                Console.Write($"{result[i]},");
+
+        }
+        public static int[] BuildArray(int[] nums)
+        {
+            int[] output = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                output[i] = nums[nums[i]];
+
+            }
+            //GC.Collect();
+            return output;
+        }
+    }
+}
